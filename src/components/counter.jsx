ba09a64559253e91a863419679f1a02dcbf17fd0
rkshristi123 <shristikumari759@gmail.com>
2022-05-19 22:23:01 +0530

    added counterapp

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Counter=({value})=>{
+
+    const [count,setCount] =React.useState(value)
+    const clr={}
+    if(count%2==1){
+        clr.color="red"
+    }
+    else{
+        clr.color="green"
+    }
+    
+    return(
+        <div>
+          <h1> Counter App : <span style={clr}>{count}</span></h1>
+          <button onClick={()=>setCount(count+1)}>increase</button>
+          <button  onClick={()=>setCount(count-1)}>decrease</button>
+         <button onClick={()=>setCount(count*2)}>double</button>
+        </div>
+    )
+}
+export default Counter
